dbd95a14ca0cfc1ac53fed33ae5f30e8c1767e0f
Giovanna Monti <ladyarcadia98@live.it>
2022-03-26 18:47:31 +0100

    Add css reset and fonts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,9 @@ export const Home = () => {
     <div>
       <MenuMobile />
       width: {width} ~ height: {height}
+      <h1>Prova h1</h1>
+      <h3>Prova h3</h3>
+      <p>Prova paragrafo</p>
     </div>
   )
 }
